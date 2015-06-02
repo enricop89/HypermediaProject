41de5316de0ecffe878ef604eec0aa8c9be3555f
Enricop89 <enrico.portolan@gmail.com>
2015-06-02 12:39:32 +0200

    Course page almost finished

--- WEBSITE/js/getCourses.js
+++ WEBSITE/js/getCourses.js
@@ -15,11 +15,16 @@ function GetURLParameter(sParam){
 
 
 function Ready() {
-    var parameter;
+    var parameter="";
     if(GetURLParameter('id')!==null && GetURLParameter('id')!==undefined)
         parameter= "?id=" + GetURLParameter('id');
     else
-        parameter="";
+         window.setTimeout(function(){
+
+        // Move to a new location or you can do something else
+        window.location.href = "http://globogym.altervista.org/404.html";
+
+    }, 500);
     
     console.log(parameter);
     $.ajax({
@@ -39,7 +44,31 @@ function Ready() {
                     $(".center").append(course_header);
                     $("#target-course").append("<h3>" +course[i].Target+ "</h3>");
                     $("#room-course").append("<h3>" +course[i].Room+ "</h3>");
-                     $("#category-course").append("<h3>" +course[i].CategoryId + "</h3>");
+                    $("#category-course").append("<h3>" +course[i].CategoryId + "</h3>");
+                    $("#item1").attr("style","background-image: url(../img/" + course[i].ImgLink1 + ")");
+                    $("#item2").attr("style","background-image: url(../img/" + course[i].ImgLink2 + ")");
+                    $("#item3").attr("style","background-image: url(../img/" + course[i].ImgLink3 + ")");
+                }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert("Status: " + textStatus); alert("Error: " + errorThrown);
+            }
+        });
+    $.ajax({
+            method: "GET",
+            dataType: "jsonp", //type of data
+            crossDomain: true,  //localhost purposes
+            url: "http://globogym.altervista.org/php/getSchedule.php" + parameter, //Relative or absolute path to                                                                                     file.php file
+            //data: {number:value},
+            success: function(response) {
+                console.log(response);
+                var schedule = response;
+                var table="<thead><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th><th>Sunday</th></thead>"
+                for(i=0; i<schedule.length; i++){
+                 table+="<tbody><td>" + schedule[i].Monday + "</td>" + "<td>" + schedule[i].Tuesday + "</td>" +  "<td>" + schedule[i].Wednesday + "</td>" + "<td>" + schedule[i].Thursday + "</td>" +  "<td>" + schedule[i].Friday + "</td>"  + "<td>" + schedule[i].Saturday + "</td>"  + "<td>" + schedule[i].Sunday + "</td>" + "</tbody>"   
+                   
+                    console.log(table);
+                    $("#schedule_table").append(table);
                 }
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
---
+++ WEBSITE/php/getSchedule.php
@@ -0,0 +1,60 @@
+<?php
+//get all the course from db and reply using json structure
+header("Access-Control-Allow-Origin: *");
+//echo "I'm the php";
+
+//connection to db
+$mysqli = new mysqli("localhost", "globogym", "", "my_globogym");
+
+if (mysqli_connect_errno()) { //verify connection
+    echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
+    exit(); //do nothing else 
+}
+else {
+    //echo "Successful connection"; // connection ok
+	if (isset($_REQUEST['id']))
+	{
+		$id = mysql_real_escape_string($_REQUEST['id']);
+		$query = "SELECT * FROM Schedule WHERE ScheduleId = {$id}";
+		
+		$result = $mysqli->query($query);
+		
+		if($result->num_rows >0)
+		{
+			$myArray = array();//create an array
+			while($row = $result->fetch_array(MYSQL_ASSOC)) {
+				array_push($myArray, array_map('utf8_encode', $row));
+			}
+			$callback = $_GET['callback'];
+			$json = json_encode($myArray);
+			echo "{$callback}({$json})";
+		}
+	}
+	else
+	{
+		# extract results mysqli_result::fetch_array
+		$query = "SELECT * FROM Schedule";
+		//query execution
+		$result = $mysqli->query($query);
+		//if there are data available
+		if($result->num_rows >0)
+		{
+			$myArray = array();//create an array
+			while($row = $result->fetch_array(MYSQL_ASSOC)) {
+				array_push($myArray, array_map('utf8_encode', $row));
+			}
+			$callback = $_GET['callback'];
+			$json = json_encode($myArray);
+			echo "{$callback}({$json})";
+		}
+
+		//free result
+		$result->close();
+
+		//close connection
+		$mysqli->close();
+	}
+}
+
+
+?>
