ee46a0d88c6bd69b791ecf39d2dabd90123727da
Enricop89 <enrico.portolan@gmail.com>
2015-06-08 10:48:32 +0200

    instructor php added

---
+++ WEBSITE/php/instructor.php
@@ -0,0 +1,57 @@
+<?php
+//get all the course from db and reply using json structure
+header("Access-Control-Allow-Origin: *");
+//echo "I'm the php";
+
+//connection to db
+$mysqli = new mysqli("localhost", "globogym", "", "my_globogym");
+
+if (mysqli_connect_errno()) { //verify connection
+    echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
+    exit(); //do nothing else
+}
+else {
+    //echo "Successful connection"; // connection ok
+	if (isset($_REQUEST['id']))
+	{
+		$id = mysql_real_escape_string($_REQUEST['id']);
+		$query = "SELECT * FROM Instructor WHERE InstructorId = {$id}";
+
+		$result = $mysqli->query($query);
+
+		if($result->num_rows >0)
+		{
+
+			$callback = $_GET['callback'];
+			$json = json_encode($result->fetch_array(MYSQL_ASSOC));
+			echo "{$callback}({$json})";
+		}
+	}
+	else
+	{
+		# extract results mysqli_result::fetch_array
+		$query = "SELECT * FROM Instructor";
+		//query execution
+		$result = $mysqli->query($query);
+		//if there are data available
+		if($result->num_rows >0)
+		{
+			$myArray = array();//create an array
+			while($row = $result->fetch_array(MYSQL_ASSOC)) {
+				array_push($myArray, array_map('utf8_encode', $row));
+			}
+			$callback = $_GET['callback'];
+			$json = json_encode($myArray);
+			echo "{$callback}({$json})";
+		}
+
+		//free result
+		$result->close();
+
+		//close connection
+		$mysqli->close();
+	}
+}
+
+
+?>
---
+++ WEBSITE/php/instructorCourses.php
@@ -0,0 +1,39 @@
+<?php
+//get all the course from db and reply using json structure
+header("Access-Control-Allow-Origin: *");
+//echo "I'm the php";
+
+//connection to db
+$mysqli = new mysqli("localhost", "globogym", "", "my_globogym");
+
+if (mysqli_connect_errno()) { //verify connection
+    echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
+    exit(); //do nothing else
+}
+else {
+    //echo "Successful connection"; // connection ok
+	if (isset($_REQUEST['id']))
+	{
+		$id = mysql_real_escape_string($_REQUEST['id']);
+		$query = "SELECT * FROM Course WHERE InstructorId = {$id}";
+
+		$result = $mysqli->query($query);
+
+		$myArray = array();//create an array
+		while($row = $result->fetch_array(MYSQL_ASSOC)) {
+			array_push($myArray, array_map('utf8_encode', $row));
+		}
+		$callback = $_GET['callback'];
+		$json = json_encode($myArray);
+		echo "{$callback}({$json})";
+	}
+	else
+	{
+		$callback = $_GET['callback'];
+		$json = json_encode(array());
+		echo "{$callback}({$json})";
+	}
+}
+
+
+?>
